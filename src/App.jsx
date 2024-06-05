427db83341ed2654227b3187e8e1d9a055b05f1f
Maciek <maciejsdev@gmail.com>
2024-06-05 15:53:40 +0200

    final form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+import { ColorList } from "./components/ColorList";
+import { Form } from "./components/Form";
+import Values from "values.js";
+import { ToastContainer, toast } from "react-toastify";
+
 const App = () => {
-  return <h2>Color Generator Starter</h2>;
+  const [colors, setColors] = useState(new Values("#f15025").all(5));
+
+  const addColor = (color) => {
+    try {
+      const newColors = new Values(color).all(5);
+      setColors(newColors);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+  return (
+    <main>
+      <Form addColor={addColor} />
+      <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
+    </main>
+  );
 };
 export default App;
